f967c1b026cdf531ec347a3b198acaecfd9846ef
Carlos Andrés Castilla Garcia <carlos.castilla@evolutionscsas.com>
2020-07-29 22:19:55 -0500

    .

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -20,7 +20,8 @@ namespace WebApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                       .UseUrls("http://0.0.0.0:44344");
                 });
     }
 }
